e305d446658bc660513888cf12bdc91d4ed45a14
akankshasme1 <shubhra@digiprima.com>
2024-02-17 17:54:23 +0530

    login section

--- controller/usercontroler.js
+++ controller/usercontroler.js
@@ -1,14 +1,18 @@
 import { query } from "express";
 import connection from "../connection.js";
 import jwt from "jsonwebtoken";
+import { message } from "@hapi/hawk/lib/client.js";
 
 export const usersignup = (req, res) => {
     let user = req.body;
     let query = "SELECT * FROM users WHERE email=?";
     connection.query(query, [user.email], (error, results) => {
+
+
         if (error) {
             return res.status(500).json({ message: "Internal server error" });
         }
+        console.log(results,'hhhhhhhhhhhhhhhhhh')
         if (results.length <= 0) {
             query = "INSERT INTO users (name, contact_number, email, password, status, role) VALUES (?, ?, ?, ?, ?, ?)";
             connection.query(query, [user.name, user.contact_number, user.email, user.password, user.status, user.role], (error, results) => {
@@ -25,20 +29,65 @@ export const usersignup = (req, res) => {
 
 
 // export const login = (req, res) => {
-//     const { email } = req.body;
-//     const query = "SELECT * FROM users WHERE email=?";
-//     connection.query(query, [email], (err, result) => {
-//         if (!err) {
-//             if(result.length <=0 || result[0].password !=password){
-//                 return res.status(401).json({message:"incorret useremail or password"})
-//             }
-//         } else {
-//             if (result.length > 0) {
-//                 const user = result[0];
-//                 const token = jwt.sign({ email: user.email, id: user.id }, 'secretkey', { expiresIn: '1h' });
-//                 res.status(200).json({ message: "Login successful", token: token });
-//             } else {
-//                 res.status(404).json({ message: "User not found" });
-//             }module.exports = login;
+//     const user = req.body;
 
+//     const query = "SELECT * FROM users WHERE email=?";
+//     connection.query(query, [user.email], (err, results) => {
+//         console.log(results,'****************')
+
+//         if(!err){
+
+//             const response = { email: results[0].email, role: results[0].role };
+//             const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
+//             res.status(200).json({ token: accessToken }); 
+
+//         }else if(results.length <= 0 || results[0].password != user.password){
+//         return res.status(401).json({ message: "Incorrect email or password" });
+
+//         }
+
+//         // if (err) {
+//         //     console.error("Error executing query:", err);
+//         //     return res.status(500).json({ message: "Internal server error" });
+//         // }
+        
+//         // if (results.length <= 0 || results[0].password != user.password) {
+//         //     return res.status(401).json({ message: "Incorrect email or password" });
+//         // } else if (results[0].status === "false") {
+//         //     return res.status(401).json({ message: "Wait for admin approval" });
+//         // } else {
+//         //    
+//         // }
+//         res.status(200).json({results:results})
+//     });
 // };
+
+export const login = (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password, '*******************')
+
+    const query = "SELECT * FROM users WHERE email=?";
+    connection.query(query, [email], (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err);
+            return res.status(500).json({ message: "Internal server error" });
+        }
+        if (result.length <= 0 || result[0].password !== password) {
+            return res.status(401).json({ message: "Incorrect email or password" });
+        }
+        if (result[0].status === "false") {
+            return res.status(401).json({ message: "Wait for admin approval" });
+        }
+        
+        const response = { email: result[0].email, role: result[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
+        res.status(200).json({ token: accessToken });
+    });
+};
+
+
+
+
+
+
+
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import  express from "express";
 
-import { usersignup } from "../controller/usercontroler.js";
+import { login, usersignup } from "../controller/usercontroler.js";
 
 const router=express.Router();
 
 router.post('/signup',usersignup)
+router.post('/login',login)
 
 export default router;
 
